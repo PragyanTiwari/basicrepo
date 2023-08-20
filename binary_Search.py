0c26a92498c3ace4c73b442cbf42a69be9c67d38
Pragyan Tiwari <tiwaripragyan099@gmail.com>
2023-08-21 02:24:24 +0530

    third commit

---
+++ binary_Search.py
@@ -0,0 +1,13 @@
+def binary_search(arr,item,low,high):
+    mid = (low+high)//2
+
+    if high >= low:
+       
+        if arr[mid] == item:
+            return mid
+        elif arr[mid] >= item:
+            return binary_search(arr,item,low,mid-1)
+        else:
+            return binary_search(arr,item,mid+1,high)
+     
+    return -1
